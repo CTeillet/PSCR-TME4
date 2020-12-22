823f2a9980a7cffc0f72b6bbca8567e4cdd6ce66
CTeillet <teillet.corentin@neuf.fr>
2020-12-22 11:55:13 +0100

    fin

--- src/Banque.cpp
+++ src/Banque.cpp
@@ -5,13 +5,19 @@
 using namespace std;
 
 namespace pr {
-
+	std::mutex m;
 void Banque::transfert(size_t deb, size_t cred, unsigned int val) {
+	m.lock();
+	comptes[deb].lock();
+	comptes[cred].lock();
 	Compte & debiteur = comptes[deb];
 	Compte & crediteur = comptes[cred];
 	if (debiteur.debiter(val)) {
 		crediteur.crediter(val);
 	}
+	comptes[deb].unlock();
+	comptes[cred].unlock();
+	m.unlock();
 }
 size_t Banque::size() const {
 	return comptes.size();
@@ -19,6 +25,7 @@ size_t Banque::size() const {
 bool Banque::comptabiliser (int attendu) const {
 	int bilan = 0;
 	int id = 0;
+	m.lock();
 	for (const auto & compte : comptes) {
 		if (compte.getSolde() < 0) {
 			cout << "Compte " << id << " en négatif : " << compte.getSolde() << endl;
@@ -26,6 +33,7 @@ bool Banque::comptabiliser (int attendu) const {
 		bilan += compte.getSolde();
 		id++;
 	}
+	m.unlock();
 	if (bilan != attendu) {
 		cout << "Bilan comptable faux : attendu " << attendu << " obtenu : " << bilan << endl;
 	}
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,46 @@
 #include "Banque.h"
+#include <iostream>
 
 using namespace std;
+//using namespace pr;
 
-const int NB_THREAD = 10;
-int main () {
+const int NB_THREAD = 20;
+const int NB_CMP = 1000;
+const int SOLDE_BASE = 1000;
+
+void transaction(pr::Banque& b) {
+	int i = ::rand() % b.size();
+	int j = ::rand() % b.size();
+	int m = ::rand() % 101 + 1;
+	b.transfert(i, j, m);
+	int r = ::rand() % 2000;
+	std::this_thread::sleep_for(std::chrono::milliseconds(r));
+}
+
+void comptabiliser(pr::Banque& b) {
+	b.comptabiliser(NB_CMP * SOLDE_BASE);
+}
+
+int main() {
 	vector<thread> threads;
 	// TODO : creer des threads qui font ce qui est demandé
+	pr::Banque b = pr::Banque(NB_CMP, SOLDE_BASE);
+	::srand(::time(nullptr));
 
+	thread comptable(comptabiliser, std::ref(b));
 
-	for (auto & t : threads) {
+	for (int i = 0; i < NB_THREAD; ++i) {
+		threads.emplace_back(transaction, std::ref(b));
+		//std::cout << i << std::endl;
+	}
+	for (auto& t : threads) {
 		t.join();
 	}
 
-	// TODO : tester solde = NB_THREAD * JP
+	comptable.join();
+
+	// TODO : tester solde = NB_THREAD * SOLDE_BASE
+	//std::cout << std::boolalpha << b.comptabiliser(NB_CMP*SOLDE_BASE) << std::endl;
 	return 0;
 }
+
